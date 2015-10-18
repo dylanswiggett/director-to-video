058aab3c4b8dd222e129ae469b5f0c08363c8560
Chad Price <chadsprice@gmail.com>
2015-10-18 10:15:32 -0700

    Made mouth masking more readable

--- ffmpeg_create_video.py
+++ ffmpeg_create_video.py
@@ -71,7 +71,9 @@ def draw_mouth(mouth, character, x, y, width, height):
     fit_height, fit_width = fit_image.shape[0:2]
     y_offset = y + (height - fit_height)
     x_offset = x + (width - fit_width) / 2
-    character[y_offset:(y_offset+fit_height), x_offset:(x_offset+fit_width)] = cv2.bitwise_and(character[y_offset:(y_offset+fit_height), x_offset:(x_offset+fit_width)], fit_mask) + cv2.bitwise_and(fit_image, cv2.bitwise_not(fit_mask))
+    y0, y1 = y_offset, (y_offset+fit_height)
+    x0, x1 = x_offset, (x_offset+fit_width)
+    character[y0:y1,x0:x1] = cv2.bitwise_and(character[y0:y1,x0:x1], fit_mask) + cv2.bitwise_and(fit_image, cv2.bitwise_not(fit_mask))
 
 def fit_dimensions(img, fit_width, fit_height):
     image_height, image_width = img.shape[0:2]
