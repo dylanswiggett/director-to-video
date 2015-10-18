131c29232144231a8577d878c14784ff9509c34e
Ryan Russell <russellsprouts2@gmail.com>
2015-10-17 20:40:31 -0700

    Generate wav files from lines

---
+++ voice.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+VOICE=$1
+PITCH=$2
+VOLUME=$3
+TEXT=$4
+
+espeak -s 140 -v mb-$VOICE -q -p $PITCH --pho --phonout=tmp/tmp.pho "$TEXT"
+mbrola -v $VOLUME /usr/share/mbrola/$VOICE/$VOICE tmp/tmp.pho tmp/tmp.wav 
